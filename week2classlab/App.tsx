1811f0590031baed374c9ff1bcc6ecf51947c9ce
BeLazy167 <dhruvkhara167@gmail.com>
2024-09-19 20:03:46 -0400

    Class lab of week 2 uploaded

---
+++ week2classlab/App.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+    StyleSheet,
+    View,
+    ScrollView,
+    Text,
+    ActivityIndicator,
+    Image,
+} from "react-native";
+
+export default function App() {
+    return (
+            <ScrollView>
+                <Text style={styles.text}>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Nullam in dui mauris.
+                </Text>
+                <Text style={styles.text}>
+                    Vivamus hendrerit arcu sed erat molestie vehicula. Sed
+                    auctor neque eu tellus rhoncus ut eleifend nibh porttitor.
+                </Text>
+                <ActivityIndicator size="large" color="#000" />
+                <Image
+                    source={{
+                        uri: "https://reactnative.dev/img/tiny_logo.png",
+                    }}
+                    style={styles.image}
+                />
+            </ScrollView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#f5f5f5",
+    },
+    scrollContainer: {
+        flexGrow: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        padding: 20,
+    },
+    text: {
+        fontSize: 16,
+        color: "#333",
+        marginBottom: 20,
+        textAlign: "center",
+    },
+    image: {
+        width: 50,
+        height: 50,
+    },
+});
